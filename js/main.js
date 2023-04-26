2f38db03ebab2da01c6cf22c5eea8d222adaa38e
Вадик <80910969+KreeDo-code@users.noreply.github.com>
2023-04-26 15:24:28 +0300

    26.04

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const btnDarkMode = document.querySelector(".dark-mode-btn");
 
-// 1. Проверка темной темы на уровне системных настроек
+// Перевірка темної теми на рівні системних налаштувань
 if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ) {
     btnDarkMode.classList.add("dark-mode-btn--active");
 	document.body.classList.add("dark");
 }
 
-// 2. Проверка темной темы в localStorage
+// Перевірка темної теми в localStorage
 if (localStorage.getItem('darkMode') === 'dark') {
     btnDarkMode.classList.add("dark-mode-btn--active");
     document.body.classList.add("dark");
@@ -15,7 +15,7 @@ if (localStorage.getItem('darkMode') === 'dark') {
     document.body.classList.remove("dark");
 }
 
-// Если меняются системные настройки, меняем тему
+// Якщо змінюються системні налаштування, змінюємо тему
 window
     .matchMedia("(prefers-color-scheme: dark)")
     .addEventListener("change", (event) => {
@@ -32,7 +32,7 @@ window
 		}
     });
 
-// Включение ночного режима по кнопке
+// Увімкнення нічного режиму за кнопкою
 btnDarkMode.onclick = function () {
     btnDarkMode.classList.toggle("dark-mode-btn--active");
     const isDark = document.body.classList.toggle("dark");
@@ -43,3 +43,57 @@ btnDarkMode.onclick = function () {
         localStorage.setItem("darkMode", "light");
     }
 };
+//
+const tabs = document.querySelector(".cpu-tabs");
+const tabButton = document.querySelectorAll(".tab-button");
+const contents = document.querySelectorAll(".content");
+
+if(tabs != null) {
+	tabs.onclick = e => {
+	  const id = e.target.dataset.id;
+	  if (id) {
+	    tabButton.forEach(btn => {
+	      btn.classList.remove("active");
+	    });
+	    e.target.classList.add("active");
+
+	    contents.forEach(content => {
+	      content.classList.remove("active");
+	    });
+	    const element = document.getElementById(id);
+	    element.classList.add("active");
+	  }
+	}
+}
+if(document.getElementById("compare_cpu_button") != null) {
+	document.getElementById("compare_cpu_button").onclick = function() {
+		var e = document.getElementById('compare_block');
+		var l = document.getElementById("compare_cpu_button");
+		e.style.display = 'flex';
+		l.style.display = 'none';
+	};
+}
+window.compareClick = function () {
+document.getElementsByName('choosed_element').forEach(item => {
+  item.addEventListener("click", function(){
+		var checkboxes = document.getElementsByName('choosed_element');
+		var selected = [];
+		for (var i=0; i<checkboxes.length; i++) {
+		    if (checkboxes[i].checked) {
+            //if(checkboxes[i].getAttribute('data-rating'))
+		        selected.push([checkboxes[i].value, checkboxes[i].getAttribute('data-id')]);
+            //console.log(selected);
+		    }
+		}
+		if(selected.length > 1) {
+			var href = window.location.href.replace(/\?page=.*/gm, '')
+      if(selected[1][1] > selected[0][1]) {
+        window.location.href = href+'/compare/'+selected[1][0]+'-vs-'+selected[0][0];
+      }
+      else {
+        window.location.href = href+'/compare/'+selected[0][0]+'-vs-'+selected[1][0];
+      }
+		}
+	}, false);
+})
+}
